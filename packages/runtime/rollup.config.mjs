b013dbaeb24d87d9816ef93952c447b63078a6c3
znydd <mailboxofnabil.com>
2025-02-14 03:54:41 +0600

    workspace packages, rollup bundler, Vitest, Eslint setup as monorepo

---
+++ packages/runtime/rollup.config.mjs
@@ -0,0 +1,14 @@
+import cleanup from 'rollup-plugin-cleanup'
+import filesize from 'rollup-plugin-filesize'
+
+export default{
+    input: 'src/index.js',
+    plugins: [cleanup()],
+    output: [
+        {
+            file: 'dist/ekok-web.js',
+            format: 'esm',
+            plugins: [filesize()],
+        }
+    ]
+}
